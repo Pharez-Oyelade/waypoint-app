5a63c633eb9e2f18a76f09370374f135ec99068f
Pharez <pharezoyelade@gmail.com>
2026-04-23 10:58:41 +0100

    minor fixes

--- server/src/utils/asyncHandler.ts
+++ server/src/utils/asyncHandler.ts
@@ -4,7 +4,9 @@
 import { Request, Response, NextFunction } from "express";
 
 export const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  <T extends Request>(
+    fn: (req: T, res: Response, next: NextFunction) => Promise<any>,
+  ) =>
+  (req: T, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
